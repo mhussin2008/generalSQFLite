4719510d2aca37ab0407e1a219e5d975371358d9
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-09-19 12:45:47 +0300

    read from database ok 2

--- lib/dbHelper/myapp_test.dart
+++ lib/dbHelper/myapp_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:generaldbase/dbHelper/theData.dart';
 import 'dbaseHelper.dart';
 
-
 class testHelper extends StatefulWidget {
   const testHelper({super.key});
 
@@ -11,48 +10,60 @@ class testHelper extends StatefulWidget {
 }
 
 class _testHelperState extends State<testHelper> {
-  dbaseHelper mydbHelper=dbaseHelper();
-  Records myRecords=Records();
+  dbaseHelper mydbHelper = dbaseHelper();
+  Records myRecords = Records();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('test helper'),
+          title: const Text('test helper'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              ElevatedButton(onPressed: (){
-                myRecords.generateData();
-                print(mydbHelper.hashCode);
-                print(mydbHelper.db.path);
-                myRecords.printData();
-              },
-              child: Text('get data'),),
-
-              ElevatedButton(onPressed: (){
-                mydbHelper.createTable();
-              },
-                  child: Text('Create Table')),
-
-              ElevatedButton(onPressed: (){
-                mydbHelper.addDatatoTable(myRecords.recordsList);
-              },
-                  child: Text('Add data to table')),
-
-              ElevatedButton(onPressed: (){
-                mydbHelper.readFromDatabase();
-              },
-                  child: Text('read data from db'))
-
-
+              ElevatedButton(
+                onPressed: () {
+                  myRecords.generateData();
+                  print(mydbHelper.hashCode);
+                  print(mydbHelper.db.path);
+                  myRecords.printData();
+                },
+                child: const Text('generate Random Data'),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    showDataFromMemory();
+                  },
+                  child: const Text('show data from memory')),
+              ElevatedButton(
+                  onPressed: () {
+                    mydbHelper.createTable();
+                  },
+                  child: const Text('Create Table')),
+              ElevatedButton(
+                  onPressed: () {
+                    mydbHelper.addDatatoTable(myRecords.recordsList);
+                  },
+                  child: const Text('Add data to table')),
+              ElevatedButton(
+                  onPressed: () {
+                    mydbHelper.readFromDatabase();
+                  },
+                  child: const Text('read data from db'))
             ],
           ),
         ),
       ),
     );
   }
+  void showDataFromMemory() {
+    myRecords.recordsList.forEach((element) {
+      print(element);
+      });
+  }
+
+
 }
