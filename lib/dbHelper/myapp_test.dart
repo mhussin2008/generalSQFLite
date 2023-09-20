03d18d49d82a95c6fed5bae379b31ee0679a2a99
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-09-20 23:22:57 +0300

    table working 3

--- lib/dbHelper/myapp_test.dart
+++ lib/dbHelper/myapp_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:generaldbase/dbHelper/tableViewerPage.dart';
+//import 'package:generaldbase/dbHelper/tableViewerPage.dart';
 import 'package:generaldbase/dbHelper/testHelperHome.dart';
 import 'package:generaldbase/dbHelper/theData.dart';
-import 'dbaseHelper.dart';
+//import 'dbaseHelper.dart';
 
 class testHelper extends StatefulWidget {
   const testHelper({super.key});
@@ -14,7 +14,12 @@ class testHelper extends StatefulWidget {
 class _testHelperState extends State<testHelper> {
   // dbaseHelper mydbHelper = dbaseHelper();
   // Records myRecords = Records();
+@override
+void initState() {
 
+    super.initState();
+    mydbHelper.readFromDatabase();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:generaldbase/Archieve/myApp.dart';
 
-import 'package:generaldbase/main.dart';
-import 'package:generaldbase/myApp.dart';
+//import 'package:generaldbase/main.dart';
+//import 'package:generaldbase/myApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
