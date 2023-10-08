847516b8d4489fb585e8d4ab5dda169c0d822a52
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-10-08 08:31:17 +0300

    added StringList save and load 5 static functions

--- lib/hive_test/hive_screen.dart
+++ lib/hive_test/hive_screen.dart
@@ -33,10 +33,36 @@ class hive_screen extends StatelessWidget {
                 hiveOperations.getdata();
               },
                 child: Text('get data from Hive',style: TextStyle(fontSize: 20)),
-              )
+              ),
+              ElevatedButton(onPressed: () async {
+                Box<dynamic> box = await initializeHive();
+
+                box.put('name', ['Ahmed','ali','refaat']);
+                box.put('age', 150);
+                box.close();
+                print('data saved');
+              },
+                child: Text('Save single data to Hive',style: TextStyle(fontSize: 20)),
+              ),
+              ElevatedButton(onPressed: () async {
+                Box<dynamic> box = await initializeHive();
+                print(box.values);
+                var data=box.get('name');
+                print(data);
+              },
+                child: Text('Get single data to Hive',style: TextStyle(fontSize: 20)),
+              ),
+
             ],
           ),
         ),),
     );
+  }
+
+  Future<Box<dynamic>> initializeHive() async {
+    var path = Directory.current.path;
+    Hive.init(path);
+    var box = await Hive.openBox('my2Box');
+    return box;
   }
 }
